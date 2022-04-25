795a0619fb950cf34d30daf7c6bb3b1251830dc4
pansplawik <splawinskikamil@gmail.com>
2022-04-25 20:54:42 +0200

    autorska strona intenetowa

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace cv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Dawno, dawno temu.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Kontakt.";
+
+            return View();
+        }
+        public ActionResult MyCV()
+        {
+            return View();
+        }
+    }
+}
